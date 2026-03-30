feb08edf61ba6c92ca35ccd63381555da2d0cd91
Moh Salauddeen <125286740+KHAN-shaan00@users.noreply.github.com>
2026-03-30 19:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let para = document.getElementById("status");
 let btn = document.getElementById("enterBtn");
 
 btn.addEventListener("click", () => {
-    para.outerHTML = '<h1 id="status">Entered Metaverse</h1>';
+    para.innerHTML = "<h1>Entered Metaverse</h1>";
 });
