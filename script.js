bde9a0ea309055feb7bb5a257813b7a6b9e9a869
Moh Salauddeen <125286740+KHAN-shaan00@users.noreply.github.com>
2026-03-30 19:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let para=document.getElementById("p1");
+let btn=document.getElementById("b1");
+btn.addEventListener("click",()=>{
+    para.innerHTML="<h1>Entered Mataverse</h1>"
+})
